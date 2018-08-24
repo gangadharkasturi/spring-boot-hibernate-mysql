165b1e514ebb7548802f8aae012ab294563366c4
Ranjith <ranjith.kandi06@gmail.com>
2018-08-23 23:09:17 -0400

    Added Logger and null check

--- src/main/java/com/datasys/springboothibernate/controller/PersonController.java
+++ src/main/java/com/datasys/springboothibernate/controller/PersonController.java
@@ -2,6 +2,8 @@ package com.datasys.springboothibernate.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,8 @@ import com.datasys.springboothibernate.model.Person;
 
 @RestController
 public class PersonController {
+	
+	Logger logger = LoggerFactory.getLogger(PersonController.class);
 
 	@Autowired
 	private PersonDao dao;
@@ -20,7 +24,10 @@ public class PersonController {
 	@PostMapping(value="/savePerson")
 	public String savePerson(@RequestBody Person person)
 	{
+		if(null != person.getName()) {
 		dao.saveUser(person);
+		logger.info("Saved Person Name: " +person.getName());
+		}
 		return "success!!";
 	}
 	@GetMapping("/getAll")
